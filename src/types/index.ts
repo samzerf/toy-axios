a987b545525e45b9ff514efd83c8b113b5b2e87d
samzherf <samzherf@gmail.com>
2021-04-22 21:55:32 +0800

    feat: 处理request headers

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,14 +1,23 @@
-export type Method = 'get' | 'GET'
-  | 'delete' | 'DELETE'
-  | 'post' | 'POST'
-  | 'put' | 'PUT'
-  | 'head' | 'HEAD'
-  | 'options' | 'OPTIONS'
-  | 'patch' | 'PATCH'
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'patch'
+  | 'PATCH'
 
 export interface AxiosRequestConfig {
   url: string
   method?: Method
   data?: any
   params?: any
+  headers?: any
 }
